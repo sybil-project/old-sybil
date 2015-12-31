aa3903f3d33896a81cec98a9d80f8bab1e884747
Shane Kilkelly <shane@kilkelly.me>
2015-12-31 20:18:24 +0000

    initial commit

---
+++ env/dev/cljs/sybil/dev.cljs
@@ -0,0 +1,4 @@
+(ns ^:figwheel-no-load sybil.dev
+  (:require [sybil.core :as core]))
+
+(core/init!)
---
+++ env/prod/cljs/sybil/prod.cljs
@@ -0,0 +1,4 @@
+(ns ^:figwheel-no-load sybil.prod
+  (:require [sybil.core :as core]))
+
+(core/init!)
